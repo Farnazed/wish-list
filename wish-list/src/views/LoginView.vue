d772bfec514af80e340823e9438797e460cb0d64
farnazed <farnaz.e.daneshvar@gmail.com>
2024-03-05 13:48:30 -0700

    tailwind setup

---
+++ wish-list/src/views/LoginView.vue
@@ -0,0 +1,11 @@
+<script setup>
+const name = defineModel()
+const password = defineModel()
+</script>
+
+<template>
+  <div class="flex flex-row items-start">
+    <input v-model="name" placeholder="name"/>
+    <input v-model="password" placeholder="password"/>
+  </div>
+</template>
