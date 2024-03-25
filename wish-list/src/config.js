189a12ae82877e0ee08db5d405e9a7a225cc1144
farnazed <farnaz.e.daneshvar@gmail.com>
2024-03-25 11:13:18 -0600

    authentication

--- wish-list/src/config.js
+++ wish-list/src/config.js
@@ -1,17 +1,17 @@
 const environmentConstants =
-  import.meta.env.VITE_NODE_ENV === "production"
+  import.meta.env.PROD === "production"
     ? {
-        NODE_ENV: import.meta.env.VITE_NODE_ENV,
-        API_URL: "",
+        NODE_ENV: import.meta.env.PROD,
+        API_URL: "http://54.174.72.35:3081",
       }
-    : import.meta.env.VITE_NODE_ENV === "production"
+    : import.meta.env.PROD === "production"
     ? {
-        NODE_ENV: import.meta.env.VITE_NODE_ENV,
+        NODE_ENV: import.meta.env.PROD,
         API_URL: "",
       }
     : {
-        NODE_ENV: import.meta.env.VITE_NODE_ENV,
-        API_URL: "http://localhost:3081",
+        NODE_ENV: import.meta.env.PROD,
+        API_URL: "http://54.174.72.35:3081",
       };
 
 export const { NODE_ENV, API_URL } = { ...environmentConstants };
